ec4ddc2b3d22fe00c6fa358ffd7e2aa2012dfc83
jupo13 <ghghtygh@naver.com>
2024-07-30 22:15:44 +0900

    test: userService 테스트 코드 추가

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,21 +1,29 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.CertificationCodeNotMatchedException;
 import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
 import com.example.demo.repository.UserEntity;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
-import java.util.UUID;
-
+import static com.example.demo.model.UserStatus.ACTIVE;
+import static com.example.demo.model.UserStatus.PENDING;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test-application.properties")
 @SqlGroup({
         @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
         @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
@@ -24,6 +32,9 @@ class UserServiceTest {
 
     @Autowired
     UserService userService;
+
+    @MockBean
+    JavaMailSender mailSender;
 
     @Test
     void getByEmail은_ACTIVE_상태인_유저를_찾아올_수_있다() {
@@ -73,4 +84,72 @@ class UserServiceTest {
         }).isInstanceOf(ResourceNotFoundException.class);
     }
 
+    @Test
+    void userCreateDto를_이용하여_유저를_생성할_수_있다() {
+        //given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("t3@t.com")
+                .address("Seoul")
+                .nickname("jupo15")
+                .build();
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        //when
+        UserEntity byId = userService.getById(1);
+        UserEntity userEntity = userService.create(userCreateDto);
+
+        //then
+        assertThat(userEntity.getId()).isNotNull();
+        assertThat(userEntity.getStatus()).isEqualTo(PENDING);
+    }
+
+    @Test
+    void userUpdateDto를_이용하여_유저를_수정할_수_있다() {
+        //given
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .address("Incheon")
+                .nickname("jupo13-2")
+                .build();
+
+        //when
+        UserEntity userEntity = userService.update(1, userUpdateDto);
+
+        //then
+        assertThat(userEntity.getId()).isEqualTo(1);
+        assertThat(userEntity.getAddress()).isEqualTo("Incheon");
+        assertThat(userEntity.getNickname()).isEqualTo("jupo13-2");
+        assertThat(userEntity.getStatus()).isEqualTo(ACTIVE);
+    }
+
+    @Test
+    void user가_로그인되면_마지막_로그인_시간이_변경된다() {
+        //given
+        //when
+        userService.login(1);
+
+        //then
+        UserEntity userEntity = userService.getById(1);
+        assertThat(userEntity.getLastLoginAt()).isGreaterThan(0L);
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_인증_코드로_ACTIVE_시킬_수_있다() {
+        //given
+        //when
+        userService.verifyEmail(2, "00000-000000-000001");
+
+        //then
+        UserEntity userEntity = userService.getById(2);
+        assertThat(userEntity.getStatus()).isEqualTo(ACTIVE);
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_잘못된_인증_코드를_받으면_예외가_발생한다() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            userService.verifyEmail(2, "00000-000000-000000");
+        }).isInstanceOf(CertificationCodeNotMatchedException.class);
+    }
 }
