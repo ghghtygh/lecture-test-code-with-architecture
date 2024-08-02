756bee737e922d4f9e33dcfdd29cf9b1a3ee27f7
jupo13 <ghghtygh@naver.com>
2024-08-03 05:28:50 +0900

    test: 헬스체크, 유저 컨트롤러 테스트 코드 추가

---
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,108 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserUpdateDto;
+import com.example.demo.repository.UserEntity;
+import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void 사용자는_특정_유저의_정보를_전달_받을_수_있다() throws Exception {
+        mockMvc.perform(get("/api/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("t@t.com"))
+                .andExpect(jsonPath("$.nickname").value("jupo13"))
+                .andExpect(jsonPath("$.status").value("ACTIVE"))
+                .andExpect(jsonPath("$.address").doesNotExist());
+    }
+
+    @Test
+    void 사용자는_존재하지_않는_유저의_아이디로_api_호출할_경우_404_응답을_받는다() throws Exception {
+        mockMvc.perform(get("/api/users/-213"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Users에서 ID -213를 찾을 수 없습니다."));
+    }
+
+    @Test
+    void 사용자는_인증_코드로_계정을_활성화_시킬_수_있다() throws Exception {
+        mockMvc.perform(get("/api/users/2/verify")
+                        .queryParam("certificationCode", "00000-000000-000001"))
+                .andExpect(status().is3xxRedirection());
+
+        UserEntity userEntity = userRepository.findById(2L).get();
+        assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test
+    void 사용자는_내_정보를_불러올_때_개인정보인_주소도_갖고_올_수_있다() throws Exception {
+        mockMvc.perform(get("/api/users/me")
+                        .header("EMAIL", "t@t.com"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("t@t.com"))
+                .andExpect(jsonPath("$.nickname").value("jupo13"))
+                .andExpect(jsonPath("$.status").value("ACTIVE"))
+                .andExpect(jsonPath("$.address").value("Seoul"));
+    }
+
+    @Test
+    void 사용자는_내_정보를_수정할_수_있다() throws Exception {
+
+        //given
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .nickname("jupo13-2")
+                .address("Daejeon")
+                .build();
+
+        //when
+        //then
+        mockMvc.perform(put("/api/users/me")
+                        .header("EMAIL", "t@t.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userUpdateDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("t@t.com"))
+                .andExpect(jsonPath("$.nickname").value("jupo13-2"))
+                .andExpect(jsonPath("$.status").value("ACTIVE"))
+                .andExpect(jsonPath("$.address").value("Daejeon"));
+    }
+}
